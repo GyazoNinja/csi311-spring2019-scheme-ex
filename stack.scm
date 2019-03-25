bcbe8b6b02a601de52e51a57ecafb9a80e31594f
Andy Gallo <agallojr@gmail.com>
2019-03-25 08:36:29 -0400

    updates

--- stack.scm
+++ stack.scm
@@ -5,7 +5,6 @@
     (let ((ls '()))
       (lambda (msg . args)
         (cond
-          ((eqv? msg 'empty?) (null? ls))
           ((eqv? msg 'push!)
            (set! ls (cons (car args) ls)))
           ((eqv? msg 'top) (car ls))
@@ -17,12 +16,7 @@
 (define stack1 (make-stack))
 (define stack2 (make-stack)) 
 
-(stack1 'empty?)
-(stack2 'empty?)
-
 (stack1 'push! 'a)
-(stack1 'empty?)
-(stack2 'empty?)
 
 (stack1 'push! 'b)
 (stack2 'push! 'c)
@@ -30,9 +24,7 @@
 (stack2 'top) 
 
 (stack1 'pop!)
-(stack2 'empty?)
 (stack1 'top) 
 
 (stack2 'pop!)
-(stack2 'empty?)
 
