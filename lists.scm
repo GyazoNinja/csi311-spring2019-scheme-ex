a17e1f6e90ee0f671c59aef6f00b0336d501d7b7
Andy Gallo <agallojr@gmail.com>
2019-04-02 19:29:39 -0400

    more eq examples

--- lists.scm
+++ lists.scm
@@ -14,10 +14,27 @@
 (cdddr '(a b c d))
 (cadadr '(a (b c)))
 
-
+(display "\n")
 (memq 'a '(b c a b d))
+(member 'a '(b c a b d))
 
+(eq? 'a 'a)
+(equal? "a" "a") 
+
+(memq "b" '("a" "b" "c"))
 (member "b" '("a" "b" "c"))
+
+(define msg 'push!)
+(eq? msg 'push!)
+(eqv? msg 'push!)
+(equal? msg 'push!)
+
+(display "\n")
+(define list1 (cons 'a (cons 'b '())))
+(define list2 (cons 'a (cons 'b '())))
+(eq? list1 list2)
+(eqv? list1 list2)
+(equal? list1 list2)
 
 
 
